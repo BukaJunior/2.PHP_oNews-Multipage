8f5d419e56f6d44433ffdab9d0947ffce2ff17e4
BukaJunior <juniorbuka@gmail.com>
2023-09-25 16:49:41 +0200

    Découverte des formulaire, balise, form, input, button, label + leur differents attributs

--- php/inc/contact.tpl.php
+++ php/inc/contact.tpl.php
@@ -1,9 +1,14 @@
 <h2 class="right__title">Nous contacter</h2>
         <div class="posts">
           <div class="post post--solo">
-            <p class="single">
-              TODO
-            </p>
+            
+            <form action="">
+              <label for="nom">Votre nom</label>
+              <input type="text" name="nom" id="nom" placeholder="Mon nom" required>
+              <button type="submit">Envoyer votre nom</button>
+
+            </form>
+
             <a href="./index.php" class="post__link">Back to home</a>
           </div>
         </div>
