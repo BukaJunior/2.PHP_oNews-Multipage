a2b3e1a8c363a8677f7019f26cd9a16b2656742c
BukaJunior <juniorbuka@gmail.com>
2023-10-06 14:46:08 +0200

    ajout des input de type email et number

--- php/inc/contact.tpl.php
+++ php/inc/contact.tpl.php
@@ -3,8 +3,26 @@
           <div class="post post--solo">
             
             <form action="">
-              <label for="nom">Votre nom</label>
-              <input type="text" name="nom" id="nom" placeholder="Mon nom" required>
+              <div>
+                <label for="nom">Votre nom</label>
+
+                <input type="text" name="nom" id="nom" placeholder="Mon nom" required>
+                
+              </div>
+
+              <div>
+                <label for="email">Votre email</label>
+
+                <input type="email" name="email" id="email" placeholder="monadress@exemple.com">
+              </div>
+
+              <div>
+                <label for="age">Votre age</label>
+
+                <input type="number" name="age" id="age" placeholder="19" min="18" max="150">
+              </div>
+              
+              
               <button type="submit">Envoyer votre nom</button>
 
             </form>
