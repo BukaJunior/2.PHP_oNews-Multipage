9a7be38df7af55dc203a0767ee63b04e12715ed9
BukaJunior <juniorbuka@gmail.com>
2023-10-08 16:36:13 +0200

    aperçu de quelques input tel que radio,birthday,radio,image

--- php/inc/contact.tpl.php
+++ php/inc/contact.tpl.php
@@ -13,16 +13,37 @@
               <div>
                 <label for="email">Votre email</label>
 
-                <input type="email" name="email" id="email" placeholder="monadress@exemple.com">
+                <input type="email" name="email" id="email" placeholder="monadress@exemple.com" required>
               </div>
 
               <div>
                 <label for="age">Votre age</label>
 
-                <input type="number" name="age" id="age" placeholder="19" min="18" max="150">
+                <input type="number" name="age" id="age" placeholder="19" min="18" max="90">
               </div>
               
+              <div>
+                <label for="image">Votre Image</label>
+
+                <input type="file" name="image" id="image">
+              </div>
+
+              <div>
+                <label for="password">Votre mot de passe</label>
+                <input type="password" name="password" id="password">
+              </div>
+
+              <div>
+                <label for="birthday">Votre date de naissance</label>
+                <input type="date" name="birthday" id="birthday">
+              </div>
               
+              <div>
+                <label for="Mr">Monsieur</label>
+                <input type="radio" name="civilite" id="Mr">
+                <label for="Me">Madame</label>
+                <input type="radio" name="civilite" id="Me">
+              </div>
               <button type="submit">Envoyer votre nom</button>
 
             </form>
