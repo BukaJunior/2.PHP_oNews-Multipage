771c8242b6671e4124965c7a0ffe73434b0b8bb3
BukaJunior <juniorbuka@gmail.com>
2023-10-26 23:16:56 +0200

    ajout des articles des donnes present sur la page index>main.tpl

--- php/inc/data.php
+++ php/inc/data.php
@@ -1,6 +1,55 @@
 <?php
 
 $postList = [
+    // On redéfini l'index des articles pour qu'ils commencent à 1
+    1 => [
+        'titre' => 'Lorem ipsum dolor article 1',
+        'auteur' => 'Darren Collison',
+        'texte' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maiores nobis nihil molestiae! Assumenda, repellendus quasi alias praesentium, reiciendis qui, labore dolore est vero hic voluptas atque officia dolor. Nihil vero amet repellendus. Dolorum nisi suscipit dolor dicta! Minima accusantium dignissimos, est vero vel quae inventore vitae iusto nobis. Minus sapiente ',
+        'catégorie' => 'team',
+        'date de publication' => '2018-02-10',
+        'l\'image de l\'auteur' => '../images/icon-dar.png',
+    ],
+    [
+        'titre' => 'Lorem ipsum dolor article 2',
+        'auteur' => 'John Marchill',
+        'texte' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maiores nobis nihil molestiae! Assumenda, repellendus quasi alias praesentium, reiciendis qui, labore dolore est vero hic voluptas atque officia dolor. Nihil vero amet repellendus. Dolorum nisi suscipit dolor dicta! Minima accusantium dignissimos, est vero vel quae inventore vitae iusto nobis. Minus sapiente ',
+        'catégorie' => 'news',
+        'date de publication' => '2018-03-27',
+        'l\'image de l\'auteur' => '../images/icon-john.png',
+    ],
+    [
+        'titre' => 'Lorem ipsum dolor article 3',
+        'auteur' => 'Olivier Hertz',
+        'texte' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maiores nobis nihil molestiae! Assumenda, repellendus quasi alias praesentium, reiciendis qui, labore dolore est vero hic voluptas atque officia dolor. Nihil vero amet repellendus. Dolorum nisi suscipit dolor dicta! Minima accusantium dignissimos, est vero vel quae inventore vitae iusto nobis. Minus sapiente ',
+        'catégorie' => 'news',
+        'date de publication' => '2018-05-05',
+        'l\'image de l\'auteur' => '../images/icon-ohz.png',
+    ],
+    [
+        'titre' => 'Lorem ipsum dolor article 4',
+        'auteur' => 'Jack Sparrow',
+        'texte' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maiores nobis nihil molestiae! Assumenda, repellendus quasi alias praesentium, reiciendis qui, labore dolore est vero hic voluptas atque officia dolor. Nihil vero amet repellendus. Dolorum nisi suscipit dolor dicta! Minima accusantium dignissimos, est vero vel quae inventore vitae iusto nobis. Minus sapiente ',
+        'catégorie' => 'work',
+        'date de publication' => '2018-07-31',
+        'l\'image de l\'auteur' => '../images/icon-spr.png',
+    ],
+    [
+        'titre' => 'Lorem ipsum dolor article 5',
+        'auteur' => 'Terry Rozier',
+        'texte' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maiores nobis nihil molestiae! Assumenda, repellendus quasi alias praesentium, reiciendis qui, labore dolore est vero hic voluptas atque officia dolor. Nihil vero amet repellendus. Dolorum nisi suscipit dolor dicta! Minima accusantium dignissimos, est vero vel quae inventore vitae iusto nobis. Minus sapiente ',
+        'catégorie' => 'team',
+        'date de publication' => '2018-09-02',
+        'l\'image de l\'auteur' => '../images/icon-tr.png',
+    ],
+    [
+        'titre' => 'Lorem ipsum dolor article 6',
+        'auteur' => 'Trevor Bayne',
+        'texte' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maiores nobis nihil molestiae! Assumenda, repellendus quasi alias praesentium, reiciendis qui, labore dolore est vero hic voluptas atque officia dolor. Nihil vero amet repellendus. Dolorum nisi suscipit dolor dicta! Minima accusantium dignissimos, est vero vel quae inventore vitae iusto nobis. Minus sapiente ',
+        'catégorie' => 'work',
+        'date de publication' => '2018-10-25',
+        'l\'image de l\'auteur' => '../images/icon-tre.png',
+    ]
 
 ];
 
