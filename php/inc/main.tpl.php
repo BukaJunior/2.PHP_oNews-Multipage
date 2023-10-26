cb6b60ad1fdaf6eb4b279518f49c03c62fbc5e81
BukaJunior <juniorbuka@gmail.com>
2023-10-27 00:19:09 +0200

    decouverte de la fonction substr, qui nous retourne une chaine de caractère

--- php/inc/main.tpl.php
+++ php/inc/main.tpl.php
@@ -2,18 +2,21 @@
 
 <h2 class="right__title">Latest News</h2>
         <div class="posts">
+
           <?php foreach($postList as $key => $currentPost) : ?>
+
             <article class="post">
-              <a href="" class="post__category post__category--color-team">CATEGORE</a>
-              <h3>TITRE</h3>
+              <a href="" class="post__category post__category--color-<?= $currentPost['catégorie']?>"><?= $currentPost['catégorie']?></a>
+              <h3><?= $currentPost['titre']?></h3>
               <div class="post__meta">
-                <img class="post__author-icon" src="../images/icon-dar.png" alt="">
-                <strong class="post__author">AUTEUR</strong>
-                <time datetime="2018-02-10">DATE</time>
+                <img class="post__author-icon" src="<?= $currentPost['l\'image de l\'auteur']?>" alt="">
+                <strong class="post__author"><?= $currentPost['auteur']?></strong>
+                <time datetime="2018-02-10"><?= $currentPost['date de publication']?></time>
               </div>
-              <p>TEXTE</p>
-              <a href="LIENS VERS ARTICLES" class="post__link">Continue reading</a>
+              <p><?= substr($currentPost['texte'], 0, 100) . " ..."?></p>
+              <a href="article<?= $key?>.php" class="post__link">Continue reading</a>
             </article>
+            
           <?php endforeach; ?>
             
         </div>
